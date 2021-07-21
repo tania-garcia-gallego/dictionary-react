8fe218791804543e7833d7b3b3240948a72fdecd
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-07-21 22:53:10 +0200

    API integration

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./DictionarySearch.css";
+
 
 export default function DictionarySearch() {
     let [word , setWord] = useState("");
 
+    function HandleResponse(response){
+        console.log(response.data);
+    }
+    
+
     function Search(event){
         event.preventDefault();
-        alert ( `Searching for ${word} definition`);
+        let apiUrl =  `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
+
+        axios.get(apiUrl).then(HandleResponse);
     }
 
     function HandleWord(event) {
