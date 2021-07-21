45c992e9f1c7f682e348a5abb48aad036df2eab0
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-07-21 22:13:19 +0200

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
+import DictionarySearch from "./DictionarySearch.js";
+
+//https://www.canva.com/colors/color-palettes/mountain-haze/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        React Dictionary
-      
       </header>
+      <DictionarySearch />
     </div>
   );
 }
---
+++ src/DictionarySearch.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./DictionarySearch.css";
+
+export default function DictionarySearch() {
+    let [word , setWord] = useState("");
+
+    function Search(event){
+        event.preventDefault();
+        alert ( `Searching for ${word} definition`);
+    }
+
+    function HandleWord(event) {
+        setWord(event.target.value);
+    }
+
+
+    return (<div>
+        <form onSubmit={Search}>
+            <input type="search" placeholder="Look up a word..." onChange={HandleWord}></input>
+            
+
+        </form>
+    </div>)
+}
