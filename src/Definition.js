b7d26d97fc2b5b15ac953f351991d64e2ca99885
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-08-04 11:18:22 +0200

    Created Phonetic component with false data

--- src/Definition.js
+++ src/Definition.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Definition.css";
 import Meaning from "./Meaning.js";
+import Phonetic from "./Phonetic.js";
 
 export default function Definition(props) {
     if (props.definition) {
          return  (
             <div className="Definitions">
              <h2 >{props.definition.word}</h2>
+             {props.definition.phonetics.map(function (phonetic, index) {
+                 return (
+                     <div key={index}>
+                         <Phonetic phonetic={phonetic} />
+                     </div>
+                 )
+             })}
              {props.definition.meanings.map(function(meaning, index) {
                  return (
                      <div key={index}>
