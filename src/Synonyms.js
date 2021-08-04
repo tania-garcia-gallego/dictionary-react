c1f8ccacead32b673303cb5571da9cfae25960db
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-08-04 10:17:41 +0200

    updated the synonyms folder with the real data

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Synonyms(props) {
-    return("Hello from Synonyms");
+    if (props.synonyms) {
+    return(
+        <strong>
+        <div className="Synonyms">
+            {props.synonyms.map(function(synonym, index) {
+                return <span key={index}>{synonym}{",   "}</span>;
+            }
+            )}
+
+        </div></strong>
+    );}
+    else {
+        return null;
+    }
 }
